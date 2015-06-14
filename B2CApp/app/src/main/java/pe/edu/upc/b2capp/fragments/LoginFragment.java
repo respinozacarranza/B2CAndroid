a4d1fa6f65e353ab1b8d29e1270b9a8cc7922cb9
GesekCore <andres.huaman.fs@gmail.com>
2015-06-14 12:28:15 -0500

    Login

---
+++ B2CApp/app/src/main/java/pe/edu/upc/b2capp/fragments/LoginFragment.java
@@ -0,0 +1,28 @@
+package pe.edu.upc.b2capp.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import pe.edu.upc.b2capp.R;
+
+/**
+ * Created by Andres on 13/06/2015.
+ */
+public class LoginFragment extends Fragment
+{
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+}
--- B2CApp/app/src/main/java/pe/edu/upc/b2capp/fragments/MainFragment.java
+++ B2CApp/app/src/main/java/pe/edu/upc/b2capp/fragments/MainFragment.java
@@ -3,17 +3,21 @@ package pe.edu.upc.b2capp.fragments;
 
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import pe.edu.upc.b2capp.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MainFragment extends Fragment {
+public class MainFragment extends Fragment  {
 
+
+    Button button_conectarse;
 
     public MainFragment() {
         // Required empty public constructor
@@ -26,6 +30,17 @@ public class MainFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedBundlesState){
+
+        button_conectarse = (Button) view.findViewById(R.id.)
+
+    }
+
+
+
+
 
 
 }
