6bdbe5a0b38198ed5d5ba6538a47c654f232bdc9
Jose <josec8080@gmail.com>
2015-06-17 08:08:03 -0500

    Agregada clase singleton volley para manejar los requests

---
+++ B2CApp/app/src/main/java/pe/edu/upc/b2capp/connection/RequestQueueManager.java
@@ -0,0 +1,64 @@
+package pe.edu.upc.b2capp.connection;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.LruCache;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by Jose on 16/06/2015.
+ */
+public class RequestQueueManager {
+
+    private static RequestQueueManager singleton;
+    private ImageLoader imageLoader;
+    private RequestQueue requestQueue;
+    private static Context context;
+
+    private RequestQueueManager(Context context) {
+        RequestQueueManager.context = context;
+        requestQueue = getRequestQueue();
+
+        imageLoader = new ImageLoader(requestQueue,
+                new ImageLoader.ImageCache() {
+                    private final LruCache<String, Bitmap>
+                            cache = new LruCache<>(20);
+
+                    @Override
+                    public Bitmap getBitmap(String url) {
+                        return cache.get(url);
+                    }
+
+                    @Override
+                    public void putBitmap(String url, Bitmap bitmap) {
+                        cache.put(url, bitmap);
+                    }
+                });
+    }
+
+    public static synchronized RequestQueueManager getInstance(Context context) {
+        if (singleton == null) {
+            singleton = new RequestQueueManager(context);
+        }
+        return singleton;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(context.getApplicationContext());
+        }
+        return requestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+
+    public ImageLoader getImageLoader() {
+        return imageLoader;
+    }
+}
---
+++ B2CApp/app/src/main/java/pe/edu/upc/b2capp/connection/UriConstant.java
@@ -0,0 +1,9 @@
+package pe.edu.upc.b2capp.connection;
+
+/**
+ * Created by Jose on 16/06/2015.
+ */
+public class UriConstant {
+    public final static String URL = "http://172.20.10.4:8080/B2CWS";
+    public final static String GET_FAVORITOS = "/favoritos/";
+}
