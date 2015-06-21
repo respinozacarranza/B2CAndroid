b122c0db0d032292f4a26b4db8ba71c146d950fe
respinozacarranza <respinozacarranza@gmail.com>
2015-06-20 19:20:32 -0500

    Google Maps implementado

---
+++ B2CApp/app/src/main/java/pe/edu/upc/b2capp/fragment/MapaInmueblesFragment.java
@@ -0,0 +1,22 @@
+package pe.edu.upc.b2capp.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+
+import pe.edu.upc.b2capp.R;
+
+/**
+ * Created by Renato on 6/20/2015.
+ */
+public class MapaInmueblesFragment extends Fragment{
+
+
+}
