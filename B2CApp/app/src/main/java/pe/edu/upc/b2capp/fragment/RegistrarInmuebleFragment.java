509c30ba7ec243e299b30ccdbc0a58496c879f4c
respinozacarranza <respinozacarranza@gmail.com>
2015-07-05 17:03:13 -0500

    Validaciones de registrar inmueble

--- B2CApp/app/src/main/java/pe/edu/upc/b2capp/fragment/RegistrarInmuebleFragment.java
+++ B2CApp/app/src/main/java/pe/edu/upc/b2capp/fragment/RegistrarInmuebleFragment.java
@@ -430,26 +430,5 @@ public class RegistrarInmuebleFragment extends Fragment{
         }
     }
 
-    public boolean esEmailValido(String email)
-    {
-        String regExpn =
-                "^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]{1}|[\\w-]{2,}))@"
-                        +"((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
-                        +"[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
-                        +"([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
-                        +"[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
-                        +"([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$";
-
-        CharSequence inputStr = email;
-
-        pattern = Pattern.compile(regExpn, Pattern.CASE_INSENSITIVE);
-        matcher = pattern.matcher(inputStr);
-
-        if(matcher.matches())
-            return true;
-        else
-            return false;
-    }
-
 
 }
